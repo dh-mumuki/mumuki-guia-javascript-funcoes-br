3fdfb694d7f4a4e34881f7c82323d50b5d26275b
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-03 15:50:07 -0400

    Mumuki Export on 2018-10-03 15:50:07 -0400

--- 00003_Usando Funções/test..js
+++ 00003_Usando Funções/test..js
@@ -3,9 +3,9 @@ describe("metade", () => {
     assert.equal(mitad(6), 3)
   })
   it("deveria dividir 100 na metade para termos 50", () => {
-    assert.equal(mitad(100), 50)
+    assert.equal(metade(100), 50)
   })
   it("deveria dividir 1 na metade para termos 0.5", () => {
-    assert.equal(mitad(1), 0.5)
+    assert.equal(metade(1), 0.5)
   })
 })
