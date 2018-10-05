6820085ef67751e2e97b497fa9d776aba4a860ed
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-05 12:17:03 -0400

    Mumuki Export on 2018-10-05 12:17:03 -0400

--- 00010_Mais uma função___/test..js
+++ 00010_Mais uma função___/test..js
@@ -1,5 +1,5 @@
 describe("", () => {
-  it("tem que devolver só 0 ou 1", () => {
+  it("gerarProbabilidade tem que devolver só 0 ou 1", () => {
 	var result = gerarProbabilidade()
 	assert(result >= 0 && result <= 100)
   })
